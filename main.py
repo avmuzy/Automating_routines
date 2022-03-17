41205533939887003daafcfdf84043348fc179ba
Andre <andrevmuzy@gmail.com>
2022-03-17 09:51:01 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+pyautogui.alert ("time to code ")
+pyautogui.keyDown('ctrl')
+pyautogui.keyDown('alt')
+pyautogui.keyDown('t')
+pyautogui.keyUp('ctrl')
+pyautogui.keyUp('alt')
+pyautogui.keyUp('t')
+
